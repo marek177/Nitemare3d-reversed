f2d460bfb2fdf692dab9a1398b68d1999b01b285
marek177 <156338151+marek177@users.noreply.github.com>
2026-09-23 11:58:01 +0200

    Assert USER.SAV flag audit entry

--- tests/unknown_systems_audit_test.cpp
+++ tests/unknown_systems_audit_test.cpp
@@ -4,10 +4,12 @@
 
 int main() {
     using namespace nitemare3d::re;
-    static_assert(kUnknownSystemTargets.size() == 25);
+    static_assert(kUnknownSystemTargets.size() == 26);
     static_assert(countByEvidence(Evidence::VerifiedExe) >= 2);
     static_assert(countByEvidence(Evidence::Todo) >= 5);
     assert(countByEvidence(Evidence::VerifiedData) >= 1);
     assert(countByEvidence(Evidence::Partial) >= 8);
     assert(kUnknownSystemTargets[0].anchor[0] != '\0');
+    assert(kUnknownSystemTargets.back().area == AuditArea::SaveState);
+    assert(kUnknownSystemTargets.back().evidence == Evidence::Partial);
 }
